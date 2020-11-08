0096c2cd4406f7ed9bedeb53502fd14efdafc9e3
lliebman <lillian.liebman@gmail.com>
2020-11-08 11:17:39 -0800

    fixed nullpointer, grid shows up on run

--- src/main/java/GridView.java
+++ src/main/java/GridView.java
@@ -4,7 +4,8 @@ import java.awt.*;
 public class GridView extends JComponent {
 
     private Grid grid;
-    public static final int CELL_SIZE = 30;
+    public static final int CELL_SIZE = 40;
+    public static final int SPACING_BETWEEN_CELLS = 2;
 
     public GridView(Grid grid) {
         this.grid = grid;
@@ -14,30 +15,27 @@ public class GridView extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paintGrid(g);
-        paintAliveCells(g);
     }
 
     void paintGrid(Graphics g) {
         Square[][] board = grid.getBoard();
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                g.setColor(Color.LIGHT_GRAY);
-                g.fillRect(board[x][y].getX(), board[x][y].getY(),CELL_SIZE, CELL_SIZE);
-                g.setColor(Color.BLACK);
-                g.drawRect(board[x][y].getX(), board[x][y].getY(),CELL_SIZE, CELL_SIZE);  //outline
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                Square s = board[x][y];
+                if (s.isAlive()) {
+                    g.setColor(Color.YELLOW);
+                }
+                else {
+                    g.setColor(Color.LIGHT_GRAY);
+                }
+                g.fillRect(SPACING_BETWEEN_CELLS+s.getX()*CELL_SIZE,
+                        SPACING_BETWEEN_CELLS+s.getY()*CELL_SIZE,
+                        CELL_SIZE-SPACING_BETWEEN_CELLS*SPACING_BETWEEN_CELLS,
+                        CELL_SIZE-SPACING_BETWEEN_CELLS*SPACING_BETWEEN_CELLS);
             }
         }
-    }
 
-    void paintAliveCells(Graphics g) {
-        for (Square s : grid.getAliveSquares()) {
-            g.setColor(Color.YELLOW);
-            g.fillRect(s.getX() * CELL_SIZE, s.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-            g.setColor(Color.BLACK);
-            g.drawRect(s.getX() * CELL_SIZE, s.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);  //outline
-        }
     }
-
 }
 
 //Don't forget Test
