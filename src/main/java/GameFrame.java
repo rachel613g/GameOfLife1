78a28be64eed7869fc19f7514d9b368a3d420bee
rachel613g <rachelgutmann613@gmail.com>
2020-11-09 16:38:57 -0500

    added buttonPanel to frame. added clearButton

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -2,13 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
-    JButton nextButton;
+    private JButton nextButton;
+    private JButton clearButton;
+    private JPanel buttonPanel;
+    private Grid grid;
+    private Dimension buttonMaxSize;
 
 
     public GameFrame(Grid grid,
                 GridView gridView,
                 GameMouseClickListener mouseClickListener) {
         super();
+
+        this.grid = grid;
+
         setSize(800, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Game of Life");
@@ -16,10 +23,29 @@ public class GameFrame extends JFrame {
         add(gridView, BorderLayout.CENTER);
         addMouseListener(mouseClickListener);
 
+        addButtonPanel();
+        addNextButton();
+        addClearButton();
+    }
 
+    private void addButtonPanel() {
+        buttonPanel = new JPanel();
+        buttonPanel.setVisible(true);
+        add(buttonPanel, BorderLayout.AFTER_LAST_LINE);
+    }
+
+    private void addClearButton() {
+        clearButton = new JButton("clear");
+        clearButton.setMaximumSize(buttonMaxSize);
+        clearButton.addActionListener(actionEvent -> grid.clearGrid());
+        buttonPanel.add(clearButton);
+    }
+
+    private void addNextButton() {
         nextButton = new JButton("next-->");
-        nextButton.setMaximumSize(new Dimension(20, 20));
-        add(nextButton, BorderLayout.AFTER_LAST_LINE);
+        buttonMaxSize = new Dimension(20, 20);
+        nextButton.setMaximumSize(buttonMaxSize);
+        buttonPanel.add(nextButton);
         nextButton.addActionListener(actionEvent -> grid.getNextGeneration());
     }
 }
