515de19ebe77ac00f223fb26efc8336ad4c70e7e
rachel613g <rachelgutmann613@gmail.com>
2020-11-10 12:04:25 -0500

    call repaint from mouselistener.

--- src/main/java/GameMouseClickListener.java
+++ src/main/java/GameMouseClickListener.java
@@ -3,15 +3,25 @@ import java.awt.event.MouseEvent;
 
 public class GameMouseClickListener extends MouseAdapter {
     private Grid grid;
-    public GameMouseClickListener(Grid grid)
+    private GridView gridView;
+    private int x;
+    private int y;
+    public GameMouseClickListener(Grid grid, GridView gridView)
     {
         this.grid = grid;
+        this.gridView = gridView;
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         super.mouseClicked(e);
         //translate e's x,y coords to our grid
-        grid.toggleSquare(e.getX()/grid.getWIDTH(), e.getY()/grid.getHEIGHT());
+        //x = e.getX();
+        //y = e.getY();
+        //System.out.println("Mouse click at " + x + ", " + y);
+        x = x / grid.getWIDTH();
+        y = y / grid.getHEIGHT();
+        grid.toggleSquare(x, y);
+        gridView.repaint();
     }
 }
